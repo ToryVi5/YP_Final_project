4966a5ef5a31bbf4041393756459e3249b60af57
ToryVi5 <tosha.5niki33@gmail.com>
2026-06-01 00:32:58 +0300

    Пройденный автотест

---
+++ configuration.py
@@ -0,0 +1,6 @@
+# URL
+URL_SERVICE = "https://19b1073e-6037-4a4d-b2a6-5bf3831fa8af.serverhub.praktikum-services.ru"
+# CREATE_ORDERS хранит путь к API-методу для создания нового заказа
+CREATE_ORDERS_PATH = "/api/v1/orders"
+# GET_ORDER_BY_TRACK хранит путь к API-методу для получения нового заказа.
+GET_ORDER_BY_TRACK_PATH = "/orders/track"
---
+++ data.py
@@ -0,0 +1,15 @@
+# Создание нового заказа
+
+order_body = {
+    "firstName": "Тоша",
+    "lastName": "Дракоша",
+    "address": "Загородное шоссе, 2А.",
+    "metroStation": 166,
+    "phone": "+7 987 145 55 33",
+    "rentTime": 3,
+    "deliveryDate": "2026-06-01",
+    "comment": "Жду свой ненаглядный самокат!",
+    "color": [
+        "BLACK"
+    ]
+}
